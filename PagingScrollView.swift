e1fa2ad65fe1ac6bbc73ed03c0ac7dc947242efb
Julian <geeksong@gmail.com>
2023-02-22 14:02:28 +0800

    Update PagingScrollView.swift

--- PagingScrollView.swift
+++ PagingScrollView.swift
@@ -11,10 +11,8 @@ struct PagingScrollView<Content>: UIViewRepresentable where Content: View {
     var isPagingEnabled = true
     var showsHorizontalScrollIndicator = false
     var showsVerticalScrollIndicator = false
+    var clipsToBounds = false
     var content: (UIScrollView) -> Content
-    init(@ViewBuilder content: @escaping (UIScrollView) -> Content) {
-        self.content = content
-    }
 
     func makeUIView(context: Context) -> UIScrollView {
         let scrollView = UIScrollView()
@@ -30,32 +28,37 @@ struct PagingScrollView<Content>: UIViewRepresentable where Content: View {
         hostingController.view.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor).isActive = true
         hostingController.view.widthAnchor.constraint(greaterThanOrEqualTo: scrollView.widthAnchor).isActive = true
         hostingController.view.heightAnchor.constraint(greaterThanOrEqualTo: scrollView.heightAnchor).isActive = true
-        scrollView.clipsToBounds = false
+        hostingController.view.backgroundColor = .clear
+        scrollView.clipsToBounds = clipsToBounds
         return scrollView
     }
 
     func updateUIView(_ uiView: UIScrollView, context: Context) {}
 }
 
+
 struct PagingScrollView_Previews: PreviewProvider {
     static var previews: some View {
         VStack{
-            GeometryReader { proxy in
-                PagingScrollView { scrollView in
-                    HStack(alignment: .top, spacing: 8){
-                        Color.pink
-                            .frame(width: proxy.size.width - 32)
-                        Color.blue
-                            .frame(width: proxy.size.width - 32)
-                        Color.yellow
-                            .frame(width: proxy.size.width - 32)
+            VStack{
+                GeometryReader { proxy in
+                    PagingScrollView { scrollView in
+                        LazyHStack(alignment: .top, spacing: 10) {
+                            ForEach(1...100,id:\.self) { _ in
+                                Color.pink
+                                    .frame(width: proxy.size.width - 40)
+                                    .cornerRadius(8)
+                            }
+                        }
+                        .padding(.leading,20)
                     }
-                    .padding(.horizontal,16)
+                    .frame(width: proxy.size.width - 30)
                 }
-                .frame(width: proxy.size.width - 24)
+                .padding(.vertical,20)
+                .frame(height: 300)
+                .background(Color.clear)
+                Spacer()
             }
-            .padding(.vertical,16)
-            .frame(height: 200)
             Spacer()
         }
     }
